afec0933030f4590d953f8d9751fa1eafe44ed50
Brandon Gong <brandon.gong@sjsu.edu>
2021-10-08 16:05:00 -0700

    Reverted changes to calculator.py. Kept changes to README.md.

--- calculator.py
+++ calculator.py
@@ -1,20 +1,19 @@
-class calculator:
-	def calculator(number1, number2, operator):
-		if operator in ('+','-','*','/','//','**'):
-			if operator == "+":
-				return number1 + number2
-			elif operator == "-":
-				return number1 - number2
-			elif operator == "*":
-				return number1 * number2
-			elif operator == "/":
-				return number1 / number2
-			elif operator == "//":
-				return number1 // number2
-			else: return number1 ** number2
-		else: return False
+def calculator(number1, number2, operator):
+	if operator in ('+','-','*','/','//','**'):
+		if operator == "+":
+			return number1 + number2
+		elif operator == "-":
+			return number1 - number2
+		elif operator == "*":
+			return number1 * number2
+		elif operator == "/":
+			return number1 / number2
+		elif operator == "//":
+			return number1 // number2
+		else: return number1 ** number2
+	else: return False
 
-	def parse_input():
-		equation = input("Enter equation")
-		list = equation.split( )
-		print(calculator(float(list[0]), float(list[2], list[1]))
+def parse_input():
+	equation = input("Enter equation")
+	list = equation.split( )
+	print(calculator(float(list[0]), float(list[2], list[1]))
