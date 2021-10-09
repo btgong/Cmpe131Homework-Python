4ae9f92a1a71d4cf4b094db73d95d3af7d379bfc
Brandon Gong <brandon.gong@sjsu.edu>
2021-10-08 17:50:17 -0700

    Fixed typo in computing last element in array. Incorrect parenthesis. Last line before return statement.

--- swap.py
+++ swap.py
@@ -1,6 +1,6 @@
 def swap_last_item(inputList):
 	temp = inputList[0]
 	inputList[0] = inputList[len(inputList) - 1]
-	inputList[len(inputList - 1)] = temp
+	inputList[len(inputList) - 1] = temp
 
 	return inputList
